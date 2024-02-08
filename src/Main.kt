9e3f32d86759675bc19a5da9ed3c8accb6290c0b
ingabirefanny <fannyingabiree@gmail.com>
2024-02-08 08:01:46 +0300

    variables

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var studentnames =("Fannyingabire")
+    println(studentnames)
+    var age =("23")
+    println(age)
+    var phonenumber =("0789153222")
+    println(phonenumber)
+    var weight =("70.5")
+    println(weight)
+    val kenyanCitizen = false
+    println(kenyanCitizen)
+}
